e7c8850d6fc04f67c9a59282c757aa1f9b8c1b7c
Rilnga <nuelagyei992@gmial.com>
2023-03-10 11:06:50 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: must_be_immutable
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyAppExt());
+  }
+}
+
+// Touch
+
+class MyAppExt extends StatefulWidget {
+  const MyAppExt({super.key});
+
+  @override
+  State<MyAppExt> createState() => _MyAppExtState();
+}
+
+class _MyAppExtState extends State<MyAppExt> {
+  String buttonname = 'Click';
+  int currentIndex = 0;
+  bool _isClicked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title Bar'),
+        leading: Icon(Icons.menu),
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Color.fromARGB(94, 97, 97, 97),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Color.fromARGB(255, 24, 17, 22),
+                        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          buttonname = 'Done';
+                        });
+                      },
+                      child: Text(buttonname),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => const Nextpage(),
+                          ),
+                        );
+                        setState(() {
+                          buttonname = 'Collect';
+                        });
+                      },
+                      child: const Text('Next page'),
+                    )
+                  ],
+                ),
+              )
+            : GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: _isClicked
+                    ? Image.asset('images/1h.jpg')
+                    : Image.asset('images/2h.jpg'),
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              label: 'Settings', icon: Icon(Icons.settings)),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+// I needed to go down
+
+// No touch
+class Nextpage extends StatelessWidget {
+  const Nextpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
